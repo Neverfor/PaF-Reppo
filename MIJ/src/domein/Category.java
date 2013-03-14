0f96dd6ff153ff887bc2ad332938cc9dd9bec45f
Michael de Vreeze <michaeldevreeze@gmail.com>
2013-03-14 11:35:51 +0100

    Repository add methods

--- MIJ/src/domein/Category.java
+++ MIJ/src/domein/Category.java
@@ -1,5 +1,52 @@
 package domein;
 
+import java.util.HashSet;
+
 public class Category {
+	private HashSet<Pattern> patterns;
+	private String name;
+	private Category childs;
+	
+	
+	/**
+	 * @return the patterns
+	 */
+	public HashSet<Pattern> getPatterns() {
+		return patterns;
+	}
+	/**
+	 * @param patterns the patterns to set
+	 */
+	public void setPatterns(HashSet<Pattern> patterns) {
+		this.patterns = patterns;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the childs
+	 */
+	public Category getChilds() {
+		return childs;
+	}
+	/**
+	 * @param childs the childs to set
+	 */
+	public void setChilds(Category childs) {
+		this.childs = childs;
+	}
+	
+	public void addChild(Category child) {
+		this.childs.addChild(child);
+	}
 
 }
--- MIJ/src/repository/Repository.java
+++ MIJ/src/repository/Repository.java
@@ -1,6 +1,49 @@
 package repository;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import domein.*;
+
 public class Repository {
 
 
+	private HashSet<Category> categories;
+	private HashSet<Pattern> patterns;
+	private PersistenceFactory persistenceType;
+	
+	/**
+	 * Adds category to Repository's list of categories.
+	 * @param newCategory
+	 * @param parents list with parents
+	 */
+	public void addCategory(Category newCategory, ArrayList<Category> parents){
+		if(parents != null){
+			for(Category category: this.categories){
+				//TODO: to get this contains() working we might need to override the equals() of Category, not tested yet
+				if(parents.contains(category)){ 
+					category.addChild(newCategory);
+				}else{
+					throw new IllegalArgumentException("Parent category not found in repository");
+				}
+			}
+		}else{
+			this.categories.add(newCategory);
+		}
+	}
+	
+	/**
+	 * Adds pattern to the list of patterns and to his categories
+	 * @param newPattern
+	 * @param categories list of catogies to put the pattern in
+	 */
+	//TODO: list of patterns might not be needed, filling it as redounded anyway
+	public void addPattern(Pattern newPattern, ArrayList<Category> categories){	
+		if(!this.patterns.add(newPattern))
+			throw new IllegalArgumentException("Pattern was already defined in repository");
+		for(Category category: categories){
+			if(category.getPatterns().contains(newPattern))
+				throw new IllegalArgumentException("Pattern was already part of " + category.getName());
+		}
+	}
 }
