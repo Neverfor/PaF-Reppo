61bf5d7bf2cc89c4ecf8615456334c9236a75579
Jan-Willem Hoekman <Jan-Willem Hoekman@jwhflaptop>
2013-03-24 14:01:10 +0100

    start nu vanaf main.java

--- MIJ/src/userinterface/MainWindow.java
+++ MIJ/src/userinterface/MainWindow.java
@@ -27,7 +27,7 @@ public class MainWindow {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -38,13 +38,14 @@ public class MainWindow {
 				}
 			}
 		});
-	}
+	}*/
 
 	/**
 	 * Create the application.
 	 */
 	public MainWindow() {
 		initialize();
+		frame.setVisible(true);
 	}
 
 	/**
