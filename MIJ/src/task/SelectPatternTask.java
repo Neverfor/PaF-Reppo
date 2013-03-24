ea2f68aec67504d8e2e8ccda684691869f497596
Neverfor <icelance@hotmail.ru>
2013-03-24 20:22:31 +0100

    selectore

--- MIJ/src/task/SelectPatternTask.java
+++ MIJ/src/task/SelectPatternTask.java
@@ -1,14 +1,26 @@
 package task;
 
+import java.io.InvalidObjectException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
+
 import repository.Repository;
 import domein.Category;
+import domein.Consequences;
+import domein.Context;
+import domein.Pattern;
+import domein.Problem;
 
 public class SelectPatternTask extends JFrame{
+	
+	private ArrayList<Category> categories = new ArrayList<Category>();
+	private ArrayList<Pattern> selectedPatterns = new ArrayList<Pattern>();
 	
 	public void fillCategoryCombobox(JComboBox<Category> cb){
 		Repository rp = Repository.getInstance();
@@ -18,5 +30,23 @@ public class SelectPatternTask extends JFrame{
 			cb.addItem(category);
 		}
 		
+	}
+	
+	public void selectPattern(String category)  {	
+		for (Category c : categories) {
+			if(c.getName().equalsIgnoreCase(category)){
+				HashSet hSet = new HashSet();
+				hSet = c.getPatterns();
+				Iterator iter = hSet.iterator();
+				 while(iter.hasNext()) 
+			      {
+			         System.out.print(iter.next());
+			         iter.remove();
+			      }
+			}
+			
+			}
+		
+	
 	}
 }
