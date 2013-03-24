ebf12951683ec3b6a3f15b7800f63e5169e9acb4
Neverfor <icelance@hotmail.ru>
2013-03-24 21:23:29 +0100

    selectPattern layout_knop_actionListener

--- MIJ/src/userinterface/SelectPattern.java
+++ MIJ/src/userinterface/SelectPattern.java
@@ -6,6 +6,7 @@ import javax.swing.GroupLayout.*;
 import javax.swing.LayoutStyle.*;
 
 
+import repository.Repository;
 import task.AddNewPatternTask;
 import task.SelectPatternTask;
 
@@ -25,12 +26,24 @@ public class SelectPattern extends JPanel {
 		
 		JComboBox<Category> cbCategory = new JComboBox<Category>();
 		task.fillCategoryCombobox(cbCategory);
-		cbCategory.setBounds(162, 73, 112, 24);
+		cbCategory.setBounds(162, 73, 130, 24);
 		add(cbCategory);
 		
 		JLabel lblCategory = new JLabel("Category:");
 		lblCategory.setBounds(32, 78, 112, 15);
 		add(lblCategory);
+		
+		JButton selectPattern = new JButton("Select Pattern");
+		selectPattern.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Repository.getInstance().getPatterns();
+			}
+		});
+
+		selectPattern.setBounds(162, 115, 130, 19);
+		add(selectPattern);
 		
 
 	}
