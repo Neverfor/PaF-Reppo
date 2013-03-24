a9555898ac2f1e5b75563c09da8b13fca09cae21
Jan-Willem Hoekman <Jan-Willem Hoekman@jwhflaptop>
2013-03-24 13:32:47 +0100

    scherm met add pattern

--- MIJ/src/userinterface/MainWindow.java
+++ MIJ/src/userinterface/MainWindow.java
@@ -3,6 +3,7 @@ package userinterface;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
@@ -11,7 +12,12 @@ import javax.swing.JOptionPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
+
+import repository.Repository;
+
 import java.awt.BorderLayout;
+
+import domein.*;
 
 public class MainWindow {
 
@@ -45,6 +51,8 @@ public class MainWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		Category c = new Category("Creational");
+		Repository.getInstance().addCategory(c, null);
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,22 +60,23 @@ public class MainWindow {
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		
+		panel = new JPanel();
+		panel.setBounds(0, 0, 200, 200);
+		
 		JMenu mnNewMenu = new JMenu("Patterns");
 		menuBar.add(mnNewMenu);
 		
 		JMenuItem mntmNewMenuItem = new JMenuItem("Add Pattern");
 		mntmNewMenuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				JOptionPane.showMessageDialog(null, this, "try", 0);
 				frame.getContentPane().remove(panel);
 				panel = new AddPattern();
 				frame.getContentPane().add(panel, BorderLayout.CENTER);
+				frame.getContentPane().revalidate();
 			}
 		});
 		mnNewMenu.add(mntmNewMenuItem);
 		
-		panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
 	}
 
 }
